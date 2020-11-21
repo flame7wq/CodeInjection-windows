a9297586b9cd414694ffe0ff0e790050d72da41f
flame7wq <54255036+flame7wq@users.noreply.github.com>
2020-11-21 11:37:47 +0800

    Update CodeInjection.cpp

--- CodeInjection.cpp
+++ CodeInjection.cpp
@@ -1,7 +1,4 @@
-﻿// CodeInjection.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
 
-#include <iostream>
 #include <Windows.h>
 typedef struct
 {
@@ -14,7 +11,7 @@ typedef struct
     DWORD dwFlagsAndAttributes;                 // file attributes
     HANDLE hTemplateFile;                       // handle to template file
 }CREATEFILE_PARAM;
-// 函数指针
+
 typedef HANDLE(WINAPI* PFN_CreateFile)(
     LPCSTR lpFileName,                         // file name
     DWORD dwDesiredAccess,                      // access mode
@@ -43,7 +40,7 @@ DWORD __stdcall CreateFileThreadProc(LPVOID lParam)
     return 0;
 }
 // 远程创建文件
-BOOL RemoteCreateFile(DWORD dwProcessID, const char* szFilePathName)
+BOOL RemoteCreateFile(DWORD dwProcessID, char* szFilePathName)
 {
     BOOL bRet;
     DWORD dwThread;
@@ -83,7 +80,7 @@ BOOL RemoteCreateFile(DWORD dwProcessID, const char* szFilePathName)
     GCreateFile.hTemplateFile = NULL;
 
     // 4. 获取CreateFile 地址
-    hModule = LoadLibraryA("kernel32.dll");
+    hModule = LoadLibrary("kernel32.dll");
     GCreateFile.dwCreateAPIAddr = (DWORD)GetProcAddress(hModule, "CreateFileA");
     FreeLibrary(hModule);
     // 5. 初始化CreateFile 文件名
@@ -112,18 +109,8 @@ BOOL RemoteCreateFile(DWORD dwProcessID, const char* szFilePathName)
 
 int main()
 {
-    CreateFileA("D:\\A.txt", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-    //RemoteCreateFile(18624, "C:\\Users\\AAAAAAA\\source\\repos\\ConsoleApplication3\\Release\\a.txt");
-    std::cout << "Hello World!\n";
+    RemoteCreateFile(6024, "C:\\new\\test.txt");
+	//CreateFileA("C:\\new\\A.txt", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
